261255d45fd135be0c45d59ec5740a6db373c010
Oleksandr Novozhylov <onovozhylov@mirantis.com>
2022-07-04 13:22:44 +0300

    Fix terraform version

--- modules/azure/main.tf
+++ modules/azure/main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.5.0"
+      version = "~> 3.5.0"
     }
     local = {
       source  = "hashicorp/local"
@@ -13,7 +13,7 @@ terraform {
     }
   }
 
-  required_version = "~> 1.1.6"
+  required_version = "~> 1.2.1"
 }
 
 provider "azurerm" {
